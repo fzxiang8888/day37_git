3939c05230e8524357ab7b0fca9810489aeeade6
fzxiang8888 <51064837+fzxiang8888@users.noreply.github.com>
2019-05-28 14:27:30 +0800

    lsgjlhg

--- src/com/qf/test/App.java
+++ src/com/qf/test/App.java
@@ -3,8 +3,8 @@ package com.qf.test;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("hshhhhh");
-        //System.out.println("jsjfosfj");
+        System.out.println("hshh");
+        System.out.println("jsjfosfj");
        // System.out.println("000");
         int a = 10;
     }
