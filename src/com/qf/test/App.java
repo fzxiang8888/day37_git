e864c5d7bc02becc5b15e814c782ab4fef385fe8
renr <123456@aliyun.com>
2019-05-28 11:48:44 +0800

    change new info

--- src/com/qf/test/App.java
+++ src/com/qf/test/App.java
@@ -3,10 +3,10 @@ package com.qf.test;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("hshhhhh");
-        System.out.println("jsjfosfj");
+        System.out.println("8888");
+        System.out.println("6666");
        // System.out.println("000");
-
+        int a = 10;
     }
 
 
