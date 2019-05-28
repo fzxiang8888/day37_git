7964dff0d4d5e61430925ccc634fe368f99ab570
renr <123456@aliyun.com>
2019-05-28 11:31:16 +0800

    delete

--- src/com/qf/test/App.java
+++ src/com/qf/test/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("hshhhhh");
         System.out.println("jsjfosfj");
-
+        System.out.println("000");
 
     }
 
