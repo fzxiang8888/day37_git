41557c750b12e03c87e869c0a6034dd36b5a8c5b
fzxiang8888 <51064837+fzxiang8888@users.noreply.github.com>
2019-05-28 14:23:42 +0800

    Update App.java

--- src/com/qf/test/App.java
+++ src/com/qf/test/App.java
@@ -4,7 +4,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("hshhhhh");
-        System.out.println("jsjfosfj");
+        //System.out.println("jsjfosfj");
        // System.out.println("000");
         int a = 10;
     }
